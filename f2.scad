883095370024408947a9af30a90c39df1cbb3393
Wouter van Ooijen <wouter@voti.nl>
2020-02-19 22:01:15 +0100

    update

---
+++ f2.scad
@@ -0,0 +1,2 @@
+linear_extrude( 15.000000 )
+   circle( r=3.000000, $fn=32 );
--- power.py
+++ power.py
@@ -1,27 +1,61 @@
+"""
+pcf8591
+
+Python library for generating OpenScad
+
+home: https://www.github.com/wovo/3d
+"""
+
 from __future__ import annotations
 from functools import reduce
 
-number_of_facets = 32
+# the default number of facets for a circle
+number_of_circle_facets = 32
 
 def ssum( x ):
-   return "".join( x )
+    """catenate a sequence of strings
+    """
+    return "".join( x )
+
+    
+#============================================================================
+# 
+# 2d and 3d ADTs
+#
+#============================================================================
 
 class xy:
+    """2d vector
+    
+    This is a 2d (x,y) vector.
+    """
 
-   def __init__( self, x, y ):
-      self.x, self.y = x, y
+    def __init__( self, x, y ):
+        """create from x and y values
+        """
+        self.x, self.y = x, y
    
-   def __add__( self, rhs: xy ):
-      return xy( self.x + rhs.x, self.y + rhs.y )
+    def __add__( self, rhs: xy ) -> xy:
+        """add two xy values
+        """
+        return xy( self.x + rhs.x, self.y + rhs.y )
       
-   def __mul__( self, v ):
-      return xy( self.x * v, self.y * v )
+    def __mul__( self, v ) -> xy:
+        """multiply an xy by a scalar
+        """
+        return xy( self.x * v, self.y * v )
       
-   def __str__( self ):
-      return( "[ %f, %f ]" % ( self.x, self.y ) )  
+    __rmul__ = __mul__
       
-   def __call__( self, rhs: element ):   
-      return element_shift( xyz( self.x, self.y, 0 ), rhs )            
+    def __str__( self ) -> string:
+        """convert to [ x, y ] string format
+        """
+        return( "[ %f, %f ]" % ( self.x, self.y ) )  
+      
+    def __call__( self, rhs: element ) -> element:   
+        """shift (translate) the argument 
+        """    
+        return element_shift( xyz( self.x, self.y, 0 ), rhs )            
 
 class xyz:
 
@@ -33,6 +67,8 @@ class xyz:
       
    def __mul__( self, v ):
       return xyz( self.x * v, self.y * v, self.z * v )
+      
+   __rmul__ = __mul__      
 
    def __str__( self ):
       return( "[ %f, %f, %f ]" % ( self.x, self.y, self.z ) )  
@@ -49,6 +85,13 @@ def dup3( x ):
 def set_z( a, z ):
    return xyz( a.x, a.y, z )   
 
+
+#============================================================================
+# 
+# elements: something that you can render in OpenScad
+#
+#============================================================================
+
 class element:
 
    def __init__( self, parts = [] ):
@@ -62,6 +105,9 @@ class element:
       
    def __sub__( self, rhs: element ):
       return element_subtract( self, rhs )
+      
+   def __mul__( self, rhs: element ):
+      return element_multiply( self, rhs )
       
    def _indent( self, txt: string ):
       return ssum( map(
@@ -93,7 +139,18 @@ class element_subtract( element ):
             self._indent( str( self.a ) + "\n" + str( self.b ) + "\n" ) +
          "}" )
          
-
+class element_multiply( element ):
+         
+   def __init__( self, a: element, b: element ):
+      element.__init__( self )
+      self.a, self.b = a, b
+      
+   def __str__( self ):
+      return ( 
+         "intersection(){\n" +
+            self._indent( str( self.a ) + "\n" + str( self.b ) + "\n" ) +
+         "}" )
+         
 class element_shift( element ):
 
    def __init__( self, direction : xyz, minion : element ):    
@@ -106,26 +163,78 @@ class element_shift( element ):
          ( "translate( %s )\n" % str( self.direction ) ) +
             self._indent( str( self.minion ) )
       )
- 
-class rectangle( element ): 
+      
+class element_extrude( element ):
+
+   def __init__( self, z, minion : element ):    
+      element.__init__( self )
+      self.minion = minion
+      self.z = z
+
+   def __str__( self ):      
+      return ( 
+         ( "linear_extrude( %f )\n" % self.z ) +
+            self._indent( str( self.minion ) )
+      )
+      
+class element_rotate( element ):
+
+   def __init__( self, d, minion : element ):    
+      element.__init__( self )
+      self.minion = minion
+      self.d = d
+
+   def __str__( self ):      
+      return ( 
+         ( "rotate( %s )\n" % str( self.d ) ) +
+            self._indent( str( self.minion ) )
+      )
+      
+      
+#============================================================================
+# 
+# OpenScad shapes
+#
+#============================================================================
+
+class primitive( element ):
+
+   def __init__( self, text ):
+      self.text = text
+      
+   def __str__( self ):
+      return self.text     
+      
+class rectangle( primitive ): 
+
+   def __init__( self, dimensions: xy ):
+      primitive.__init__( self, 
+         "square( %s );" % str( dimensions ))         
+      
+class box( primitive ): 
 
    def __init__( self, dimensions: xyz ):
-      element.__init__( self )
-      self.dimensions = dimensions
+      primitive.__init__( self, 
+         "cube( %s );" % str( dimensions ))
       
-   def __str__( self ):
-      return "cube( %s );" % str( self.dimensions )
-      
-class cylinder( element ): 
+class circle( primitive ): 
 
-   def __init__( self, r, h, f = number_of_facets ):
-      element.__init__( self )
-      self.r = r
-      self.h = h
-      self.f = f
-      
-   def __str__( self ):
-     return "cylinder( r=%f, h=%f, $fn=%d );" % ( self.r, self.h, self.f )
+   def __init__( self, r, f = number_of_circle_facets ):
+      primitive.__init__( self, 
+          "circle( r=%f, $fn=%d );" % ( r, f ))          
+     
+class cylinder( primitive ): 
+
+   def __init__( self, r, h, f = number_of_circle_facets ):
+      primitive.__init__( self, 
+          "cylinder( r=%f, h=%f, $fn=%d );" % ( r, h, f ))          
+     
+     
+#============================================================================
+# 
+# element manipulators
+#
+#============================================================================     
      
 class repeat4:
 
@@ -139,18 +248,60 @@ class repeat4:
          xy(      0, self.y ) ( rhs ),
          xy( self.x, self.y ) ( rhs ),
       ] )   
+      
+class extrude:
+
+   def __init__( self, z ):
+      self.z = z   
          
+   def __call__( self, rhs: element ):   
+      return element_extrude( self.z, rhs )
+           
+class rotate:
+
+   def __init__( self, d ):
+      self.d = d   
+         
+   def __call__( self, rhs: element ):   
+      return element_rotate( self.d, rhs )
+           
+           
+#============================================================================
+# 
+# derived shapes
+#
+#============================================================================     
+
+def chisel( size ):
+   return rectangle( size ) - dup2( size)( circle( size ) )
+      
+def rounded_rectangle( size: xy, rounding ) -> element:
+   ch = chisel( rounding )
+   return rectangle( size ) - element( [
+      ch,
+      xy( 0, size.y )( rotate( xyz( 0, 0, -90 )))( ch ),
+   ] )   
+      
+      
+#============================================================================
+# 
+# 
+#
+#============================================================================     
+      
       
 c = repeat4( 10, 30 ) ( cylinder( 5, 15 ) - cylinder( 2, 15 ) )
 cx = xy( 0, 0 ) ( cylinder( 5, 15 ) - cylinder( 2, 15 ) )
-c.print( "f.scad" )
+extrude( 15 )( circle( 3 ) ).print( "f2.scad" )
+
+rounded_rectangle( xy( 10, 5 ), 2 ).print( "f.scad" )
       
 
 pcb = xy( 78, 83 )
 margin = 1
-wall = 1
-plate = pcb + dup2( margin ) * 2 + dup2( wall ) * 2
-bottom = rectangle( set_z( plate, wall ))
-bottom.print( "f.scad" )
+wall = 8
+plate = pcb + 2 * dup2( margin ) * 2 + 2* dup2( wall )
+bottom = box( set_z( plate, wall ))
+bottom.print( "fx.scad" )
 
       
